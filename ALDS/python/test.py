0bced13d054aa73b30dbb7e62bc8e9dc9d5632cc
javateaboy <noikari73@gmail.com>
2018-12-13 16:28:52 +0900

    ベースが狂っていたので修正

--- ALDS/python/test.py
+++
@@ -1,7 +0,0 @@
-x,a,b = list(map(int, input().split()))
-if -a + b <= 0:
-    print("delicious")
-elif -a + b <= x:
-    print("safe")
-else:
-    print("dangerous")
--- ALDS/python/test2.py
+++
@@ -1,8 +0,0 @@
-n,k = map(int, input().split())
-a = list(map(int, input().split()))
-ans = 100000000 * n
-for i in range(n - k + 1):
-    l = i
-    r = i + k - 1
-    ans = min([ans, abs(a[l]) + abs(a[r] - a[l]), abs(a[r]) + abs(a[r] - a[l])])
-print(ans)
