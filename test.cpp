7593e73f7bd9f74d44be68f4d5093d44b0d2b198
javateaboy <noikari73@gmail.com>
2019-02-20 22:55:07 +0900

    no message

--- test.cpp
+++ test.cpp
@@ -1,21 +1,6 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-const double EPS = 1e-9;
-typedef vector<int> vint;
-typedef pair<int, int> pint;
-#define rep(i, n) REP(i, 0, n)
-#define ALL(v) v.begin() , v.end()
-#define REP(i, x, n) for(int i = x; i < n; i++)
+#include <iostream>
 
-
-int main() {
-	int n, a[100001], ans = 0, x;
-	cin >> n;
-	REP(i, 1, n + 1) {
-		cin >> x;
-		if (a[x] == i) ans++;
-		a[i] = x;
-	}
-	cout << ans << endl;
+int main(){
+	std::cout << "Hello, World" << std::endl;
+	return 0;
 }
