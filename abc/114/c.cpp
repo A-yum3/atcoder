9c2bb29da3aee330954a2ddb47f06970fa29b5f6
Javateaboy <36401898+Javateaboy@users.noreply.github.com>
2019-03-31 20:11:52 +0900

    a
    a

--- abc/114/c.cpp
+++ abc/114/c.cpp
@@ -7,7 +7,6 @@ const int MOD = 1e9+7;
 const ll LINF = 1e18;
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vll;
 typedef pair<int, int> pi;
 typedef pair<ll, ll> pll;
 typedef map<int, int> mi;
@@ -21,14 +20,17 @@ typedef set<int> si;
 
 #define rep(i, n) REP(i, 0, n)                              // 0, 1, ..., n-1
 #define REP(i, x, n) for(int i = x; i < n; i++)             // x, x + 1, ..., n-1
-#define invrep(i, n) for(int i = (n)-1; i >= 0; i--)        // n-1, n-2, ..., 0
-#define invREP(i, x, n) for(int i = (n)-1; i >= (x; i--)    // n-1, n-2, ..., x
 #define FOREACH(x,a) for(auto& (x) : (a) )
+#define SORT(x) sort(x.begin(), x.end())
 
 #define ALL(v) (v).begin() , (v).end()
 #define RALL(v) (v).rbegin(), (v).rend()
 
-#define PB push_back
+#define pb push_back
+#define pu push
+#define mp make_pair
+#define fi first
+#define sc second
 
 #define COUT(x) cout << (x) << endl
 #define VECCIN(x) for(auto&youso_: (x) )cin>>youso_
@@ -38,7 +40,21 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
 
+int ans = 0, N;
+
+void dfs(ll x, ll mask) {
+    if (x > N) return;
+    if (mask == 7) ans++;
+    dfs (x * 10 + 3, mask | 1);
+    dfs (x * 10 + 5, mask | 2);
+    dfs (x * 10 + 7, mask | 4);
+}
+
 int main(){
-    int N; cin >> N;
-    
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    cin >> N;
+    dfs(0, 0);
+    COUT(ans);
 }
