81025b52f31b31d82d8fefe1eab78ac0e1db496c
Javateaboy <36401898+Javateaboy@users.noreply.github.com>
2019-06-02 22:09:29 +0900

    a
    a

---
+++ abc/037/c.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+typedef vector<int> VI;
+typedef vector<vector<int>> VVI;
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PL;
+typedef map<int, int> MI;
+typedef set<int> SI;
+#define VV(T) vector<vector< T > >
+#define dump(x)  cout << #x << " = " << (x) << endl
+#define YES(n) cout << ((n) ? "YES" : "NO"  ) << endl
+#define Yes(n) cout << ((n) ? "Yes" : "No"  ) << endl
+#define POSSIBLE(n) cout << ((n) ? "POSSIBLE" : "IMPOSSIBLE"  ) << endl
+#define Possible(n) cout << ((n) ? "Possible" : "Impossible"  ) << endl
+
+#define rep(i, n) REP(i, 0, n)                              // 0, 1, ..., n-1
+#define REP(i, x, n) for(int i = x; i < n; i++)             // x, x + 1, ..., n-1
+#define FOREACH(x,a) for(auto& (x) : (a) )
+
+#define ALL(v) (v).begin() , (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define COUT(x) cout << (x) << endl
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+ll rui[111111];
+ll a[111111];
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, k; cin >> n >> k;
+    rep(i, n) cin >> a[i];
+
+    for(int i = 0; i < n; i++) {
+        rui[i + 1] = a[i];
+    }
+    for(int i = 0; i < n; i++) {
+        rui[i + 1] += rui[i];
+    }
+    ll ans = 0;
+    for(int i = k; i <= n; i++) {
+        ans += rui[i] - rui[i - k];
+    }
+    COUT(ans);
+}
