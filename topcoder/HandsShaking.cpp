9930407410d871bdf285959b9aeed8a7cfc0813a
Javateaboy <36401898+Javateaboy@users.noreply.github.com>
2019-06-04 20:37:58 +0900

    a
    a

---
+++ topcoder/HandsShaking.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define dump(x)  cout << #x << " = " << (x) << endl
+#define YES(n) cout << ((n) ? "YES" : "NO"  ) << endl
+#define Yes(n) cout << ((n) ? "Yes" : "No"  ) << endl
+#define POSSIBLE(n) cout << ((n) ? "POSSIBLE" : "IMPOSSIBLE"  ) << endl
+#define Possible(n) cout << ((n) ? "Possible" : "Impossible"  ) << endl
+
+#define rep(i, n) REP(i, 0, n)                              // 0, 1, ..., n-1
+#define REP(i, x, n) for(int i = x; i < n; i++)             // x, x + 1, ..., n-1
+#define FOREACH(x,a) for(auto& (x) : (a) )
+
+#define ALL(v) (v).begin() , (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+
+#define COUT(x) cout << (x) << endl
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+class HandsShaking {
+    public: long long countPerfect(int n) {
+        long long *dp = new long long[n / 2 + 1];
+        dp[0] = 1;
+        for(int i = 1; i <= n / 2; i++) {
+            dp[i] = 0;
+            for(int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        ll ans = dp[n / 2];
+        delete[] dp;
+        return ans;
+    }
+};
