a55274e6f05a8d6478bf0805585ec2ec7e20de2d
javateaboy <noikari73@gmail.com>
2019-06-21 11:48:35 +0900

    no message

--- test.cpp
+++ test.cpp
@@ -1,26 +1,21 @@
 #include <bits/stdc++.h>
+#include <random>
 using namespace std;
-typedef long long ll;
-#define dump(x)  cout << #x << " = " << (x) << endl
-#define YES(n) cout << ((n) ? "YES" : "NO"  ) << endl
-#define Yes(n) cout << ((n) ? "Yes" : "No"  ) << endl
-#define POSSIBLE(n) cout << ((n) ? "POSSIBLE" : "IMPOSSIBLE"  ) << endl
-#define Possible(n) cout << ((n) ? "Possible" : "Impossible"  ) << endl
+template <class RandomIter>
 
-#define rep(i, n) REP(i, 0, n)                              // 0, 1, ..., n-1
-#define REP(i, x, n) for(int i = x; i < n; i++)             // x, x + 1, ..., n-1
-#define FOREACH(x,a) for(auto& (x) : (a) )
+void bogosort( RandomIter first, RandomIter last )
+{
+    while( !is_sorted( first, last ) )
+    shuffle( first, last, default_random_engine() );
+}
 
-#define ALL(v) (v).begin() , (v).end()
-#define RALL(v) (v).rbegin(), (v).rend()
-
-#define COUT(x) cout << (x) << endl
-
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-
+int main() {
+    vector<int> kotake{5, 1, 7, 8};
+    vector<int> ans{1, 5, 7, 8};
+    int cnt = 0;
+    while(!equal(kotake.cbegin(), kotake.cend(), ans.cbegin())){
+        bogosort(kotake.begin(), kotake.end());
+        cnt++;
+    }
+    cout << cnt << endl;
 }
