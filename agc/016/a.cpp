c782e92fa66d22cbf3e12fed62691a721e1c9053
Javateaboy <36401898+Javateaboy@users.noreply.github.com>
2019-06-26 23:40:45 +0900

    agc-aume

--- agc/016/a.cpp
+++ agc/016/a.cpp
@@ -35,18 +35,30 @@ int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
-    ll n, p; cin >> n >> p;
-    int flg = 0;
-    ll ans = 0;
-    rep(i, n) {
-        ll a; cin >> a;
-        if(a & 1) flg = 1;
+    string s; cin >> s;
+    int ans = 110;
+    int f = 1;
+    // 全て同一文字の場合は0を出力
+    for(int i = 1; i < s.size(); i++) if(s[0] != s[i]) f = 0;
+    if(f) {
+        cout << 0 << endl;
+        return 0;
     }
-    if(flg) {
-        ans = pow(2, n - 1);
-    } else {
-        if(p) ans = 0;
-        else ans = pow(2, n);
+
+    // 決めた文字がi, i+1 にない場合はそのまま、存在したらそれをiに入れる
+    for(char c = 'a'; c <= 'z'; c++) { // a~zまで全探索
+        string s1 = s;
+        int flg = true;
+        int cnt = 0;
+        while(flg) {
+            flg = false;
+            for(int i = 0; i < s.size() - cnt - 1; i++) {
+                if(s1[i] == c || s1[i + 1] == c) s1[i] = c;
+                else flg = true; // この時点で全て同じ文字じゃないと判断できる
+            }
+            cnt++;
+        }
+        ans = min(ans, cnt);
     }
     cout << ans << endl;
 }
