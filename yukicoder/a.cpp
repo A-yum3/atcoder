fa7d7a3292590f18432cfe05bd314fdc55a97729
Javateaboy <36401898+Javateaboy@users.noreply.github.com>
2019-06-28 22:21:53 +0900

    a

---
+++ yukicoder/a.cpp
@@ -0,0 +1,49 @@
+/*
+　　こんにちは。あたしはｶｳｶﾞｰﾙ。
+　　redcoderになるためAtCoderを巡る旅をしてます。
+
+　　　 　 ＿_
+　　　 ヽ|＿_|ノ　　　　ﾓｫ
+　　　　||‘‐‘||ﾚ　　_)_, ―‐ 、
+　　　　/(Ｙ (ヽ＿ /・ ヽ　　 ￣ヽ
+　　　　∠ ＿ ゝ　 ｀^ヽ ﾉ.::::_(ノヽ
+　　　　 _/ヽ　 　　  /ヽ￣￣/ヽ
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define dump(x)  cout << #x << " = " << (x) << endl
+#define YES(n) cout << ((n) ? "YES" : "NO"  ) << endl
+#define Yes(n) cout << ((n) ? "Yes" : "No"  ) << endl
+#define POSSIBLE(n) cout << ((n) ? "POSSIBLE" : "IMPOSSIBLE"  ) << endl
+#define Possible(n) cout << ((n) ? "Possible" : "Impossible"  ) << endl
+
+#define rep(i, n) REP(i, 0, n)                              // 0, 1, ..., n-1
+#define REP(i, x, n) for(int i = x; i < n; i++)             // x, x + 1, ..., n-1
+#define FOREACH(x,a) for(auto& (x) : (a) )
+
+#define ALL(v) (v).begin() , (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+
+#define COUT(x) cout << (x) << endl
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string s1, s2; cin >> s1 >> s2;
+    string sat = "Sat";
+    string sun = "Sun";
+    if((s1 == sat && s2 == sun) || (s1 == sun && s2 == sat)){
+        cout << "8/33" << endl;
+    }
+    else if((s1 == sat && s2 != sun) || (s1 == sun && s2 != sat)) {
+        cout << "8/32" << endl;
+    } else {
+        cout << "8/31" << endl;
+    }
+}
