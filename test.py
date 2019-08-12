c2ff22bfec63d21f0aca33e36385d23c539d7f6d
Javateaboy <36401898+Javateaboy@users.noreply.github.com>
2019-08-12 22:09:34 +0900

    a
    a

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-a = int(input())
-if a % 2 == 0:
-    print("Blue")
-else:
-    print("Red")
+s = input()
+ans = ""
+for c in s:
+    if c == 'O': print(0)
+    elif c == 'D': print(0)
+    elif c == 'I': print(1)
+    elif c == 'Z': print(2)
+    elif c == 'S': print(5)
+    elif c == 'B': print(8)
+    else: print(c)
