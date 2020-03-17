e3e8d092a89ab20b933481bec8380651941bea0e
javateaboy <noikari73@gmail.com>
2020-03-17 16:07:35 +0900

    no message

--- cptest_atcoder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-problemname=$1
-cnt=${#problemname}-2
-oj dl "https://atcoder.jp/contests/${problemname:0:cnt}/tasks/${problemname//-/_}"
-g++ -Wall -std=c++14 ./${problemname:cnt+1}.cpp
-oj test
-rm -f a.out
-rm -rf test
